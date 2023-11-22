6cbdfb659448c4477eadc94153ff16df7cb8e63b
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2023-11-22 22:20:28 +0900

    update

---
+++ src/w11/atClass/step3/script/sketch.js
@@ -0,0 +1,38 @@
+//getUserMedia()
+
+let capture;
+function setup() {
+  setCanvasContainer('canvas', 1, 1, true);
+  let constraints = {
+    video: {
+      width: { exact: 50 },
+      height: { exact: 50 },
+      frameRate: {
+        ideal: 30,
+        min: 10,
+      },
+    },
+    audio: false,
+  };
+  capture = createCapture(constraints);
+
+  console.log(capture);
+  capture.hide();
+}
+
+function draw() {
+  //   background('white');
+  image(capture, 0, 0, width, height);
+  loadPixels();
+  background('white');
+  const gridSize = 10;
+  for (let y = 0; y < height; y += gridSize) {
+    for (let x = 0; x < width; x += gridSize) {
+      const idx = (width * y + x) * 4;
+      const norm = pixels[idx] / 255;
+      fill(0);
+      noStroke();
+      ellipse(x, y, gridSize * (1 - norm));
+    }
+  }
+}
